1b5b8a8781c932d785b31d10442bacd6c0670b3f
RicciWoo <ricciwoo@gmail.com>
2018-07-03 20:13:34 +0800

    36 thr

--- sl_gradient_3d_test.py
+++ sl_gradient_3d_test.py
@@ -29,7 +29,7 @@ ftype = moving.dtype.type
 out = np.empty(tuple(out_shape)+(dim,), dtype=ftype)
 inside = np.empty(tuple(out_shape), dtype=np.int32)
 _gradient_3d(moving, moving_world2grid, moving_spacing, 
-             static_grid2world, out, inside, 35)
+             static_grid2world, out, inside, 36)
 
 # np.save('sl_gradient_3d.npy', out)
 
