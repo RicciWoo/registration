d4f69c1d7b2225b51ffe54941a6e1df5e64670de
RicciWoo <ricciwoo@gmail.com>
2018-07-04 17:55:27 +0800

    update

--- sl_gradient_3d_test.py
+++ sl_gradient_3d_test.py
@@ -5,30 +5,31 @@ import numpy.linalg as npl
 from dipy.align.imwarp import get_direction_and_spacings
 from dipy.align.vector_fields import _gradient_3d
 
+fetch_stanford_hardi()
+nib_stanford, _ = read_stanford_hardi()
+static = np.squeeze(nib_stanford.get_data())[..., 0]
+static_grid2world = nib_stanford.affine
+
+fetch_syn_data()
+_, nib_syn_b0 = read_syn_data()
+moving = np.array(nib_syn_b0.get_data())
+moving_grid2world = nib_syn_b0.affine
+
+static = np.array(static).astype(np.float64)
+moving = np.array(moving).astype(np.float64)
+
+moving_world2grid = npl.inv(moving_grid2world)
+
+dim = len(static.shape)
+moving_direction, moving_spacing = \
+    get_direction_and_spacings(moving_grid2world, dim)
+
+out_shape = static.shape
+ftype = moving.dtype.type
+out = np.empty(tuple(out_shape)+(dim,), dtype=ftype)
+inside = np.empty(tuple(out_shape), dtype=np.int32)
+
 def grad(num_threads=0):
-	fetch_stanford_hardi()
-	nib_stanford, _ = read_stanford_hardi()
-	static = np.squeeze(nib_stanford.get_data())[..., 0]
-	static_grid2world = nib_stanford.affine
-
-	fetch_syn_data()
-	_, nib_syn_b0 = read_syn_data()
-	moving = np.array(nib_syn_b0.get_data())
-	moving_grid2world = nib_syn_b0.affine
-
-	static = np.array(static).astype(np.float64)
-	moving = np.array(moving).astype(np.float64)
-
-	moving_world2grid = npl.inv(moving_grid2world)
-
-	dim = len(static.shape)
-	moving_direction, moving_spacing = \
-	    get_direction_and_spacings(moving_grid2world, dim)
-
-	out_shape = static.shape
-	ftype = moving.dtype.type
-	out = np.empty(tuple(out_shape)+(dim,), dtype=ftype)
-	inside = np.empty(tuple(out_shape), dtype=np.int32)
 	_gradient_3d(moving, moving_world2grid, moving_spacing, 
 	             static_grid2world, out, inside, num_threads)
 
