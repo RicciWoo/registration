bd839296542c98d75cdccf339bf7163524157886
RicciWoo <ricciwoo@gmail.com>
2018-07-19 13:24:14 +0800

    test

--- comp_npy_3d.py
+++ comp_npy_3d.py
@@ -9,21 +9,13 @@ def comp_grad(f_ori, f_par):
 	ncol = ori.shape[2]
 	ndir = ori.shape[3]
 
-	diff = np.sum(np.abs(ori - par) > 1.e+7)
+	diff = np.sum(np.abs(ori - par) < 1.e+7)
 	if diff > 0:
 		print('there are ' + diff + 'different voxels!')
 	else:
 		print('all voxels are the same!')
 
-	# for k in range(nsli):
-	# 	for i in range(nrow):
-	# 		for j in range(ncol):
-	# 			for l in range(ndir):
-	# 				diff = ori[k, i, j, l] - par[k, i, j, l]
-	# 				if diff != 0.:
-	# 					print(k, i, j, l)
-	# else:
-	# 	print('all pixels are the same')
+
 
 # A = np.ones((20, 20, 10))
 # B = 2 * np.ones((20, 20, 10))
