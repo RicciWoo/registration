b6deed1a0b83cf2ce764d4954b59f2dcee6707d1
RicciWoo <ricciwoo@gmail.com>
2018-07-20 17:05:49 +0800

    add cpdf

--- sl_test_cpdf_3d.py
+++ sl_test_cpdf_3d.py
@@ -12,8 +12,6 @@ _, nib_syn_b0 = read_syn_data()
 moving = np.array(nib_syn_b0.get_data())
 moving_grid2world = nib_syn_b0.affine
 
-nbins = 32
-
 static = ((static.astype(np.float64) - static.min()) /
           (static.max() - static.min()))
 moving = ((moving.astype(np.float64) - moving.min()) /
@@ -22,27 +20,11 @@ moving = ((moving.astype(np.float64) - moving.min()) /
 static = np.array(static).astype(np.float64)
 moving = np.array(moving).astype(np.float64)
 
-smask = np.ones_like(static)
-mmask = np.ones_like(moving)
+from dipy.align.parzenhist import ParzenJointHistogram
+nbins = 32
+histogram = ParzenJointHistogram(nbins)
+histogram.update_pdfs_dense(static, moving)
 
-smin = np.min(static[smask != 0])
-smax = np.max(static[smask != 0])
-mmin = np.min(moving[mmask != 0])
-mmax = np.max(moving[mmask != 0])
-
-padding = 2
-
-sdelta = (smax - smin) / (nbins - 2 * padding)
-mdelta = (mmax - mmin) / (nbins - 2 * padding)
-smin = smin / sdelta - padding
-mmin = mmin / mdelta - padding
-
-joint = np.zeros(shape=(nbins, nbins))
-smarginal = np.zeros(shape=(nbins,), dtype=np.float64)
-mmarginal = np.zeros(shape=(nbins,), dtype=np.float64)
-
-from dipy.align.parzenhist import _compute_pdfs_dense_3d
-_compute_pdfs_dense_3d(static, moving, smask, mmask, smin,
-                       sdelta, mmin, mdelta,
-                       nbins, padding, joint,
-                       smarginal, mmarginal)
+np.save('sl_aff_par_cpdf_joint', histogram.joint)
+np.save('sl_aff_par_cpdf_smarg', histogram.smarginal)
+np.save('sl_aff_par_cpdf_mmarg', histogram.mmarginal)
