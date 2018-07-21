a7f7972f53521f652473b419d40b23c225897534
RicciWoo <ricciwoo@gmail.com>
2018-07-21 15:47:45 +0800

    update

--- comp_npy.py
+++ comp_npy.py
@@ -4,6 +4,15 @@ def comp_grad(f_ori, f_par):
 	ori = np.load(f_ori)
 	par = np.load(f_par)
 
+	n_ori = np.sum(np.abs(ori) > 1.e+7)
+	if n_ori > 0:
+		print(n_ori)
+		print('non-zero values in origin')
+	n_par = np.sum(np.abs(par) > 1.e+7)
+	if n_par > 0:
+		print(n_par)
+		print('non-zero values in compare')
+
 	diff = np.sum(np.abs(ori - par) > 1.e+7)
 	if diff > 0:
 		print(diff)
