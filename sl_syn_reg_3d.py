88413fabe8e9e8ad083eb6539038972a8c25ab87
RicciWoo <ricciwoo@gmail.com>
2018-08-05 15:32:36 -0400

    update sl_syn_reg_3d.py

--- sl_syn_reg_3d.py
+++ sl_syn_reg_3d.py
@@ -11,20 +11,16 @@ from xvfbwrapper import Xvfb
 from dipy.data import fetch_stanford_hardi, read_stanford_hardi
 fetch_stanford_hardi()
 nib_stanford, _ = read_stanford_hardi()
-stanford_b0 = np.squeeze(nib_stanford.get_data())[..., 0]
+# stanford_b0 = np.squeeze(nib_stanford.get_data())[..., 0]
 
 from dipy.data.fetcher import fetch_syn_data, read_syn_data
 fetch_syn_data()
 _, nib_syn_b0 = read_syn_data()
-syn_b0 = np.array(nib_syn_b0.get_data())
+# syn_b0 = np.array(nib_syn_b0.get_data())
 
-from dipy.segment.mask import median_otsu
-stanford_b0_masked, _ = median_otsu(stanford_b0, 4, 4)
-syn_b0_masked, _ = median_otsu(syn_b0, 4, 4)
-
-static = stanford_b0_masked
+static = np.load('stf_b0_masked.npy')
 static_affine = nib_stanford.affine
-moving = syn_b0_masked
+moving = np.load('syn_b0_masked.npy')
 moving_affine = nib_syn_b0.affine
 
 pre_align = np.array([[1.02783543e+00, -4.83019053e-02, -6.07735639e-02, -2.57654118e+00],
